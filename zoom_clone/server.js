ee2993b2afac4fc9ca0353283e7615e1857bc94e
Suresh Kumar <rskumartce@gmail.com>
2020-11-05 02:38:37 +0800

    Removing the filters.

--- zoom_clone/server.js
+++ zoom_clone/server.js
@@ -1,29 +1,72 @@
+// const express = require('express')
+// const app = express()
+// const server = require('http').Server(app)
+// const io = require('socket.io')(server)
+// const { v4: uuidV4 } = require('uuid')
+
+// app.set('view engine', 'ejs')
+// app.use(express.static('public'))
+
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidV4()}`)
+// })
+
+// app.get('/:room', (req, res) => {
+//   res.render('room', { roomId: req.params.room })
+// })
+
+// io.on('connection', socket => {
+//   socket.on('join-room', (roomId, userId) => {
+//     socket.join(roomId)
+//     socket.to(roomId).broadcast.emit('user-connected', userId)
+
+//     socket.on('disconnect', () => {
+//       socket.to(roomId).broadcast.emit('user-disconnected', userId)
+//     })
+//   })
+// })
+
+// server.listen(3000)
+
 const express = require('express')
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + "/public"))
+let clients = 0
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer')
+            }
+        }
+        else
+            this.emit('SessionActive')
+        clients++;
     })
-  })
+    socket.on('Offer', SendOffer)
+    socket.on('Answer', SendAnswer)
+    socket.on('disconnect', Disconnect)
 })
 
-server.listen(3000)
+function Disconnect() {
+    if (clients > 0) {
+        if (clients <= 2)
+            this.broadcast.emit("Disconnect")
+        clients--
+    }
+}
+
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer)
+}
+
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data)
+}
+
+http.listen(port, () => console.log(`Active on ${port} port`))
